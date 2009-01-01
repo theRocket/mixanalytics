7eaf5ca2750a28909cd9418ab27e12fa748ea61b
lars <larsburgess@gmail.com>
2008-12-31 17:52:28 -0800

    fixing client login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,22 @@ class SessionsController < ApplicationController
 
   # disable forgery protection for login
   # TODO: Only do this for json requests!
-  protect_from_forgery :except => :create
+  protect_from_forgery :except => :client_login
+  
+  before_filter :login_required, :except => :client_login
 
   # render new.rhtml
   def new
+  end
+  
+  def client_login
+    logout_keeping_session!
+    user = User.authenticate(params[:login], params[:password])
+    if user
+      self.current_user = user
+      new_cookie_flag = (params[:remember_me] == "1")
+      handle_remember_cookie! new_cookie_flag
+    end
   end
 
   def create
