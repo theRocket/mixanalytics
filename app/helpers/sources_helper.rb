d40815f9a0faf8f38391f42c0e5b9513fd828d7d
lars <larsburgess@gmail.com>
2009-01-08 08:58:45 -0800

    commenting verbose logging

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -161,14 +161,14 @@ module SourcesHelper
       
       # find the new records
       @object_values.each do |ov|
-        logger.debug "current object_value: #{ov.inspect}"
+        #logger.debug "current object_value: #{ov.inspect}"
         map = ClientMap.find_or_initialize_by_client_id_and_object_value_id({:client_id => client_id, 
                                                                              :object_value_id => ov.id,
                                                                              :object_value_object => ov.object,
                                                                              :object_value_attrib => ov.attrib,
                                                                              :object_value_value => ov.value,
                                                                              :db_operation => 'insert'})
-        logger.debug "client_map record: #{map.inspect}"                                                                     
+        #logger.debug "client_map record: #{map.inspect}"                                                                     
         if map and map.new_record?
           map.save
           map.object_value.db_operation = map.db_operation
@@ -181,8 +181,6 @@ module SourcesHelper
     maps_to_delete = ClientMap.find_all_by_client_id(client_id)
     maps_to_delete.each do |map|
       obj = map.object_value
-      #puts "COMPARING[obj]: #{obj.inspect}"
-      #puts "COMPARING[map]: #{map.inspect}"
       if obj.nil?
         temp_obj = ObjectValue.new
         temp_obj.object = map.object_value_object
