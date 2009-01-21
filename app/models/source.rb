60270ac0bd85d2ef496d4d92b72660ed9fa77b7f
adamblum <a@adamblum.com>
2009-01-20 18:16:31 -0800

    refactored the refresh method on the source model

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,4 +1,5 @@
 class Source < ActiveRecord::Base
+  include SourcesHelper
   has_many :object_values
   belongs_to :app
   attr_accessor :source_adapter,:current_user,:credential
@@ -40,110 +41,31 @@ class Source < ActiveRecord::Base
     @current_user=current_user
     initadapter
     # not all endpoints require WSDL! dont do this if you dont see WSDL in the URL (a bit of a hack)
-    client = SOAP::WSDLDriverFactory.new(url).create_rpc_driver if url and url.size>0 and url=~/wsdl/
-    source_adapter.client=client if source_adapter
-    # make sure to use @client and @session_id variables
-    # in your code that is edited into each source!
+    @client = SOAP::WSDLDriverFactory.new(url).create_rpc_driver if url and url.size>0 and url=~/wsdl/
+    source_adapter.client=@client if source_adapter
+    # make sure to use @client and @session_id variable in your code that is edited into each source!
     if source_adapter
-      source_adapter.login
+      source_adapter.login  # should set up @session_id
     else
       callbinding=eval %"#{prolog};binding"
     end
     # also you can get user credentials from credential
     usersub=app.memberships.find_by_user_id(current_user.id) if current_user
     @credential=usersub.credential if usersub # this variable is available in your source adapter
-
-    # first do all the the creates
-    creates=ObjectValue.find_by_sql("select * from object_values where update_type='create' and source_id="+id.to_s)
-    uniqobjs=creates.map {|x| x.object}
-    uniqobjs.uniq!
-    uniqobjs.each do |x|
-      xvals=ObjectValue.find_all_by_object_and_update_type(x,'create')  # this has all the attribute value pairs for this particular object
-      if xvals.size>0
-        attrvalues={}
-        xvals.each do |y|
-          attrvalues[y.attrib]=y.value if y.attrib and y.value
-          y.destroy
-        end
-        # now attrvalues has the attribute values needed for the createcall
-        # the Sugar adapter will use the name_value_list variable that we're building up here
-        # TODO: name_value_list is probably too specific to Sugar, although we've made it work with other apps
-        #  need a clean way to pass the attrvalues hash to any source adapter cleanly
-        nvlist=make_name_value_list(attrvalues)
-        if source_adapter
-          name_value_list=eval(nvlist)
-          source_adapter.create(name_value_list)
-        else
-          raise ArgumentError,"Need some create code to execute" if createcall.nil?
-          callbinding=eval("name_value_list="+nvlist+";"+createcall+";binding",callbinding)
-        end
-      end
-    end
-
-    # now do the updates
-    updates=ObjectValue.find_by_sql("select * from object_values where update_type='update' and source_id="+id.to_s)
-    uniqobjs=updates.map {|x|x.object}
-    uniqobjs.uniq!
-    uniqobjs.each do |x|
-      objvals=ObjectValue.find_all_by_object_and_update_type(x,'update')  # this has all the attribute value pairs now
-      attrvalues={}
-      attrvalues["id"]=x  # setting the ID allows it be an update
-      objvals.each do |y|
-        attrvalues[y.attrib]=y.value
-        y.destroy
-      end
-      # now attrvalues has the attribute values needed for the createcall
-      nvlist=make_name_value_list(attrvalues)
-      if source_adapter
-        name_value_list=eval(nvlist)
-        source_adapter.update(name_value_list)
-      else
-        raise ArgumentError,"Need some update code to execute" if updatecall.nil?
-        callbinding=eval("name_value_list="+nvlist+";"+updatecall+";binding",callbinding)
-      end
-    end
-
-
-    # now do the deletes
-    deletes=ObjectValue.find_by_sql("select * from object_values where update_type='delete' and source_id="+id.to_s)
-    uniqobjs=deletes.map {|x|x.object}
-    uniqobjs.uniq!
-    uniqobjs.each do |x|
-      attrvalues={}
-      attrvalues["id"]=x
-      nvlist=make_name_value_list(attrvalues)
-      if source_adapter
-        name_value_list=eval(nvlist)
-        source_adapter.delete(name_value_list)
-      else
-        raise ArgumentError,"Need some delete code to execute" if deletecall.nil?
-        callbinding=eval("name_value_list="+nvlist+";"+deletecall+";binding",callbinding)
-      end
-    end
-    deletes.each do |x|  # get rid of the deletes
-      x.destroy
-    end
-      
+    # perform core create, update and delete operations
+    process_update_type('create',createcall)
+    process_update_type('update',updatecall)
+    process_update_type('delete',deletecall)      
     # do the query call and sync of records
-    user_id=User.find_by_login credential.login if credential
+    @user_id=User.find_by_login credential.login if credential
     if source_adapter
       source_adapter.query
       source_adapter.sync
     else
       callbinding=eval(call+";binding",callbinding)
       callbinding=eval(sync+";binding",callbinding) if sync
-    end
-    
-    # if there is a user credential just manage the object value records associated with that user
-    user_id=User.find_by_login credential.login if credential
-    delete_cmd= "(update_type='query') and source_id="+id.to_s
-    (delete_cmd << " and user_id="+user_id) if user_id # if there is a credential then just do delete and update based upon the records with that credential
-    ObjectValue.delete_all delete_cmd
-    pending_to_query="update object_values set update_type='query',id=pending_id where (update_type='pending' or update_type is null) and source_id="+id.to_s
-    (pending_to_query << " and user_id="+user_id) if user_id 
-    ObjectValue.find_by_sql pending_to_query
-    updated_at=Time.new # timestamp
-
+    end 
+    finalize_query_records
     # now do the logoff
     if source_adapter
       source_adapter.logoff
@@ -152,9 +74,38 @@ class Source < ActiveRecord::Base
         callbinding=eval(epilog+";binding",callbinding)
       end
     end
-
-    refreshtime=Time.new  # keep track of the refresh time to help optimize show queries
     save
   end
   
+  def finalize_query_records
+    delete_cmd= "(update_type='query') and source_id="+id.to_s
+    (delete_cmd << " and user_id="+ @user_id) if @user_id # if there is a credential then just do delete and update based upon the records with that credential
+    ObjectValue.delete_all delete_cmd
+    pending_to_query="update object_values set update_type='query',id=pending_id where (update_type='pending' or update_type is null) and source_id="+id.to_s
+    (pending_to_query << " and user_id=" + @user_id) if @user_id 
+    ObjectValue.find_by_sql pending_to_query
+    updated_at=Time.new # timestamp    
+  end
+  
+  def process_update_type(utype,utypecall)
+    objs=ObjectValue.find_by_sql("select distinct(object) from object_values where update_type='"+ utype +"'and source_id="+id.to_s)
+    objs.each do |x|
+      objvals=ObjectValue.find_all_by_object_and_update_type(x.object,utype)  # this has all the attribute value pairs now
+      attrvalues={}
+      attrvalues["id"]=x.object if utype!='create' # setting the ID allows it be an update or delete
+      objvals.each do |y|
+        attrvalues[y.attrib]=y.value
+        y.destroy
+      end
+      # now attrvalues has the attribute values needed for the createcall
+      nvlist=make_name_value_list(attrvalues)
+      if source_adapter
+        name_value_list=eval(nvlist)
+        eval("source_adapter." +utype +"(name_value_list)")
+      else
+        (raise ArgumentError,"Need some code to execute for " + utype) if utypecall.nil?
+        callbinding=eval("name_value_list="+nvlist+";"+utypecall+";binding",callbinding)
+      end
+    end
+  end
 end
