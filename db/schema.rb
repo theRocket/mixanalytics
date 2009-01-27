c0269fb2708c96cb8026a61678cec77cd9393417
adamblum <a@adamblum.com>
2009-01-26 18:15:25 -0800

    allow there to be a URL stored on credential that can be used in source adapter code for "per user URLs" [#59 state:resolved]

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090122191208) do
+ActiveRecord::Schema.define(:version => 20090127020800) do
 
   create_table "apps", :force => true do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(:version => 20090122191208) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "membership_id"
+    t.string   "url"
   end
 
   create_table "credentials_sources", :force => true do |t|
