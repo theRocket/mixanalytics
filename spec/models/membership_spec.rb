69d9025776d3ff900d57b2b35647df582db88648
lars <larsburgess@gmail.com>
2009-02-03 08:26:52 -0800

    * cleaned up query code in process_objeccts_for_client * fixed membership spec

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,12 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe Subscription do
+describe Membership do
   before(:each) do
     @valid_attributes = {
     }
   end
 
   it "should create a new instance given valid attributes" do
-    Subscription.create!(@valid_attributes)
+    Membership.create!(@valid_attributes)
   end
 end
