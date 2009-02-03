b272cdd0b84014cca57024a5638853cc40a0d59b
adamblum <a@adamblum.com>
2009-02-03 14:28:06 -0800

    debugging why the delete doesnt work

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -48,10 +48,14 @@ module SourcesHelper
     pendings_cmd = "select pending_id from object_values where update_type is null and source_id="+id.to_s
     (pendings_cmd << " and user_id="+ credential.user.id.to_s) if credential# if there is a credential then just do delete and update based upon the records with that credential  
     pendings_cmd << " order by pending_id"
+    p "Executing " + pendings_cmd
     objs=ObjectValue.find_by_sql pendings_cmd
     prev=nil
     objs.each do |obj|  # remove dupes
-      prev.destroy if prev and (obj.pending_id==prev.pending_id)
+      if (prev and (obj.pending_id==prev.pending_id))
+        p "Deleting duplicate"
+        p.destroy
+      end
       prev=obj
     end
   end
