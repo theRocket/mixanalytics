642c3155d10018b86fa47c03c10de03289aa2764
lars <larsburgess@gmail.com>
2009-02-18 15:16:25 -0800

    trying out cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,47 @@
 set :application, "rhosync"
-set :repository,  "git://github.com/rhomobile/rhosync.git"
+set :repository,  "git@github.com:rhomobile/rhosync.git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/var/www/apps/#{application}"
+set :deploy_to, "/var/www/apps/prod/#{application}"
+
+default_run_options[:pty] = true
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :git
 
-server "apps.rhohub.com", :app, :web, :db, :primary => true
+server "rhohub.com", :app, :web, :db, :primary => true
 
 set :user, "www-data"
 
 set :branch, "master"
 set :repository_cache, "git_cache"
 set :deploy_via, :remote_cache
+
+# set :copy_strategy, :export
 set :ssh_options, { :forward_agent => true }
 
 namespace :deploy do
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    
-    run "touch #{current_path}/tmp/restart.txt"
-  end
+ desc "Restarting mod_rails with restart.txt"
+ task :restart, :roles => :app, :except => { :no_release => true } do
 
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
-  end
-end 
+   run "touch #{current_path}/tmp/restart.txt"
+ end
 
-namespace :rake do
-  desc "Load the sample data remotely"
-  task :samples do
-    run("cd #{deploy_to}/current; /usr/bin/rake db:fixtures:samples")
-  end
+ [:start, :stop].each do |t|
+   desc "#{t} task is a no-op with mod_rails"
+   task t, :roles => :app do ; end
+ end
 end
 
+namespace :rake do
+ desc "Load the sample data remotely"
+ task :samples do
+   run("cd #{deploy_to}/current; /usr/bin/rake db:fixtures:samples")
+ end
+ task :bootstrap do
+   run("cd #{deploy_to}/current; /usr/bin/rake db:bootstrap RAILS_ENV=production")
+ end
+end
