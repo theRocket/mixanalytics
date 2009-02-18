a98843b5556c52c99e8d00f8bf31ca1e835861d6
Vidal Graupera <vidal@vdggroup.com>
2009-02-17 21:45:36 -0800

    wip on siebel adapter

--- lib/siebel_service_requests.rb
+++ lib/siebel_service_requests.rb
@@ -7,8 +7,9 @@ class SiebelServiceRequests < SourceAdapter
   def login
     puts "SiebelServiceRequests login source = #{@source.inspect.to_s}"
     
-    # always log into this url https://secure.crmondemand.com/Services/Integration?command=login
-    https = Net::HTTP.new("secure.crmondemand.com",443)
+    https = Net::HTTP.new(@source.url,443)
+    https.set_debug_output $stderr
+    
     https.use_ssl = true
     https.verify_mode = OpenSSL::SSL::VERIFY_NONE
     
@@ -33,78 +34,86 @@ class SiebelServiceRequests < SourceAdapter
     start=session.index('=')
     finish=session.index(';')
     session=session[start+1...finish] if start and finish
-    endpoint_url ='https://secure-ausomxbha.crmondemand.com/Services/Integration/ServiceRequest;jsessionid='+session
+    endpoint_url ="https://#{@source.url}/Services/Integration/ServiceRequest;jsessionid=#{session}"
+    puts "endpt = #{endpoint_url}"
     
-    obj = Default_Binding_ServiceRequest.new(endpoint_url)
-    obj.wiredump_dev = STDERR
-    http=nil
+    @obj = Default_Binding_ServiceRequest.new(endpoint_url)
+    @obj.wiredump_dev = STDERR
   end
 
   def query
-    puts "SiebelServiceRequests query"
-    if obj.nil?
-      puts "obj nil" and return
+    puts "SiebelServiceRequests query with obj= #{@obj}"
+    return if @obj.nil?
+
+    begin
+      input=ServiceRequestWS_ServiceRequestQueryPage_Input.new
+      # input.StartRowNum=0
+      # input.PageSize=10
+      s=ServiceRequest.new
+      s.LastUpdated=">= 01/01/2009 00:00:00"
+      s.ServiceRequestId=""
+      # s.Subject=""
+      # s.CreatedDate=""
+      # s.AccountName=""
+      # s.Description=""
+      # s.Priority=""
+      input.ListOfServiceRequest=[s]
+      @result=@obj.serviceRequestQueryPage(input)
+      puts "returning from query"
+      puts @result.inspect.to_s
+    rescue => e
+      puts "exception #{e.inspect}"
     end
-    
-    input=ServiceRequestWS_ServiceRequestQueryPage_Input.new
-    input.startRowNum=0
-    s=ServiceRequest.new
-    s.serviceRequestId=""
-    s.subject=""
-    s.createdDate=""
-    s.accountName=""
-    s.description=""
-    s.priority=""
-    input.listOfServiceRequest=[s]
-    @result=obj.serviceRequestQueryPage(input)
   end
 
   def sync
-    puts "SiebelServiceRequests sync with #{@result.listOfServiceRequest.length}"
+    puts "SiebelServiceRequests sync with #{@result.inspect.to_s}"
 
-    @result.listOfServiceRequest.each do |x|
-      # subject
-      o=ObjectValue.new
-      o.source_id=@source.id
-      o.object=x.serviceRequestId
-      o.attrib="subject"
-      o.value=x.subject
-      o.save
-      #accountName
-      o=ObjectValue.new
-      o.source_id=@source.id
-      o.object=x.serviceRequestId
-      o.attrib="accountName"
-      o.value=x.accountName
-      o.save
-      #description
-      o=ObjectValue.new
-      o.source_id=@source.id
-      o.object=x.serviceRequestId
-      o.attrib="description"
-      o.value=x.description
-      o.save
-      #priority
-      o=ObjectValue.new
-      o.source_id=@source.id
-      o.object=x.serviceRequestId
-      o.attrib="priority"
-      o.value=x.priority
-      o.save
-      #createdDate attribute
-      o=ObjectValue.new
-      o.source_id=@source.id
-      o.object=x.serviceRequestId
-      o.attrib="createdDate"
-      o.value=x.createdDate
-      o.save
+    if @result
+      @result.listOfServiceRequest.each do |x|
+        # subject
+        o=ObjectValue.new
+        o.source_id=@source.id
+        o.object=x.serviceRequestId
+        o.attrib="subject"
+        o.value=x.subject
+        o.save
+        #accountName
+        o=ObjectValue.new
+        o.source_id=@source.id
+        o.object=x.serviceRequestId
+        o.attrib="accountName"
+        o.value=x.accountName
+        o.save
+        #description
+        o=ObjectValue.new
+        o.source_id=@source.id
+        o.object=x.serviceRequestId
+        o.attrib="description"
+        o.value=x.description
+        o.save
+        #priority
+        o=ObjectValue.new
+        o.source_id=@source.id
+        o.object=x.serviceRequestId
+        o.attrib="priority"
+        o.value=x.priority
+        o.save
+        #createdDate attribute
+        o=ObjectValue.new
+        o.source_id=@source.id
+        o.object=x.serviceRequestId
+        o.attrib="createdDate"
+        o.value=x.createdDate
+        o.save
+      end
     end
   end
 
   def create(name_value_list)
     puts "SiebelServiceRequests create #{name_value_list.inspect.to_s}"
     
-    ServiceRequestWS_ServiceRequestInsert_Input.new
+    input=ServiceRequestWS_ServiceRequestInsert_Input.new
     s=ServiceRequest.new
     s.serviceRequestId=""
     s.accountName=name_value_list[0]["value"] # account name
@@ -112,7 +121,7 @@ class SiebelServiceRequests < SourceAdapter
     s.priority=name_value_list[2]["value"] # priority
     s.subject=name_value_list[3]["value"] # subject
     input.listOfServiceRequest=[s]
-    result=obj.serviceRequestInsert(input)
+    result=@obj.serviceRequestInsert(input)
   end
 
   def update(name_value_list)
@@ -125,7 +134,7 @@ class SiebelServiceRequests < SourceAdapter
     s.priority=name_value_list[2]["value"] # priority
     s.serviceRequestId=name_value_list[3]["value"] # subject
     input.listOfServiceRequest=[s]
-    output=obj.serviceRequestUpdate(input)
+    output=@obj.serviceRequestUpdate(input)
   end
 
   def delete(name_value_list)
